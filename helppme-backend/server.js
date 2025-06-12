1c35fdf86bd7a562633bf0d2c4996f784ef9dc28
Zabiullah S <zabi2201@hotmail.com>
2025-06-12 15:14:56 +0530

    server bug fixed

--- helppme-backend/server.js
+++ helppme-backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     origin:
-      process.env.FRONTEND_URL || "http://103.235.106.138" || "admin.helppme.in" || "helppme.in" || "https://helppme-in-frontend.vercel.app",
+      process.env.FRONTEND_URL || "http://103.235.106.138" || "http://admin.helppme.in" || "https://helppme.in" || "https://helppme-in-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
@@ -53,7 +53,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin:
-      process.env.FRONTEND_URL || "http://103.235.106.138" || "admin.helppme.in" || "helppme.in" || "https://helppme-in-frontend.vercel.app", // Use the frontend URL explicitly
+      process.env.FRONTEND_URL || "http://103.235.106.138" || "http://admin.helppme.in" || "https://helppme.in" || "https://helppme-in-frontend.vercel.app", // Use the frontend URL explicitly
     methods: ["GET", "POST", "PUT"]
   },
   transports: ["websocket", "polling"] // Enable WebSocket and Polling
